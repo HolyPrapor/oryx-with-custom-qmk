b4a01da8ff892d53da9d213e4a502ab6a09fcca2
Ilya Tzoop <34509541+HolyPrapor@users.noreply.github.com>
2025-06-30 16:00:00 +0200

    Fix comment

--- 3WOgG/rules.mk
+++ 3WOgG/rules.mk
@@ -5,5 +5,5 @@ ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
 SPACE_CADET_ENABLE = no
 
-// custom for automatic alt/option tweaking and os-dependent language switch 
+# custom for automatic alt/option tweaking and os-dependent language switch 
 OS_DETECTION_ENABLE = yes
