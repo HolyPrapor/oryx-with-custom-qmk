3c75ff2b997ff12c791dfe5e2a1ac3452558ff0c
Ilya Tzoop <34509541+HolyPrapor@users.noreply.github.com>
2025-06-30 15:42:55 +0200

    Update rules.mk to allow os detection

--- 3WOgG/rules.mk
+++ 3WOgG/rules.mk
@@ -4,3 +4,7 @@ MOUSEKEY_ENABLE = no
 ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
 SPACE_CADET_ENABLE = no
+
+// custom for automatic alt/option tweaking and os-dependent language switch 
+OS_DETECTION_ENABLE = yes
+DEFERRED_EXEC_ENABLE = yes
