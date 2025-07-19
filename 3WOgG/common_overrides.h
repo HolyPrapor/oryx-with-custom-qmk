8a6e93ecdd4ab8dc43eca744e8548a85bfd8aa89
Ilya Tzoop <34509541+HolyPrapor@users.noreply.github.com>
2025-07-19 15:04:30 +0200

    Another attempt at fixing unorthodox key orders

--- 3WOgG/common_overrides.h
+++ 3WOgG/common_overrides.h
@@ -66,8 +66,11 @@ static inline bool process_common_override(uint16_t keycode, keyrecord_t *record
         if (keycode == active_override_kc) {
             /* Drop the Ctrl+key we registered on press                 */
             unregister_code16(LCTL(plain_kc));
+            if (!(real_mods & MOD_MASK_CTRL)) {
+                restore_mods_cached &= ~MOD_MASK_ALT;   // delete Alt bit because it's not held anymore
+            }
             /* Give the user’s real mods back (they’re still physically held) */
-            register_mods(restore_mods_cached & real_mods);
+            register_mods(restore_mods_cached);
             active_override_kc  = KC_NO;
             restore_mods_cached = 0;
             prev_overridden = true;
